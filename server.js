eb2713fea6f8d69461ec513aa4a5033e90e8b0cc
Msadauki <muhdsadauki2000@gmail.com>
2026-02-14 13:54:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-// Mount routes
+app.use(express.json());
+
 const downloadRoutes = require('./Routes/download');
 app.use('/', downloadRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`AppRaR backend running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`AppRaR backend running on port ${PORT}`);
+});
